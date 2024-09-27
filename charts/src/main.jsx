f2a4ca168895bf98f0ac69e3824bc99ddc7e118b
Sertan Akpinar <sert@MBP-von-Sertan.fritz.box>
2024-09-27 12:56:05 +0200

    only bar chart

--- charts/src/main.jsx
+++ charts/src/main.jsx
@@ -4,9 +4,11 @@ import App from './App.jsx'
 import FußballerChart from './FußballerChart.jsx'
 import './index.css'
 import KreisChart from './kreisChart.jsx'
+import BarChart from './BarChart.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <KreisChart />
+    <BarChart />
   </StrictMode>,
 )
